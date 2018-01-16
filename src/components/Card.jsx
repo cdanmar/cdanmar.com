7fe709c275d2422375b891507e2af1cb1581e471
cdanmar <cdanmar@gmail.com>
2018-01-16 15:24:18 -0600

    pre reactstrap

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,29 +1,12 @@
 import React, { Component } from 'react';
+import './Card.css';
 
 class Card extends Component {
     render () {
         return (
             <div className="card-deck">
                 <div className="card">
-                    <img className="card-img-top" src="http://via.placeholder.com/235x200" alt="Card"></img>
-                    <div className="card-block">
-                        <h4 className="card-title">Card title</h4>
-                        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        <p className="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                    </div>
-                </div>
-
-                <div className="card">
-                    <img className="card-img-top" src="http://via.placeholder.com/235x200" alt="Card"></img>
-                    <div className="card-block">
-                        <h4 className="card-title">Card title</h4>
-                        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        <p className="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                    </div>
-                </div>
-
-                <div className="card">
-                    <img className="card-img-top" src="http://via.placeholder.com/235x200" alt="Card"></img>
+                    <img className="card-img-top" src="https://source.unsplash.com/320x240" alt="Card"></img>
                     <div className="card-block">
                         <h4 className="card-title">Card title</h4>
                         <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
