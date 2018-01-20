9b7da07b174b39250e23c73b9451381e5681b750
cdanmar <cdanmar@gmail.com>
2018-01-19 23:40:31 -0600

    added navbar navigation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,7 +17,7 @@ class Home extends Component {
           <div className="outer-div">
             <div className="card-deck">
               <Card link="/about" image="https://source.unsplash.com/jay5BqVyf5A/320x200" title="About Me / Contact" text="Learn more about who I am, and how to reach out" />
-              <Card link="/about" image="https://source.unsplash.com/cckf4TsHAuw/320x200" title="My Work" text="See what I have been up to"/>
+              <Card link="/portfolio" image="https://source.unsplash.com/cckf4TsHAuw/320x200" title="My Work" text="See what I have been up to"/>
               <Urlcard url="http://resume.cdanmar.com/" image="https://source.unsplash.com/31hjRkCJrdo/320x200" title="My Resume" text="A link to my most current resume" />
             </div>
           </div>
